bd61ca78c3159a477cd31255c16673336f3b8f3c
0m3re <100145279+0m3re@users.noreply.github.com>
2022-11-02 09:51:10 +0100

    Create rectangle.java

---
+++ Programmierung1/ueb00/rectangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class rectangle {
+    public static void main(String[] args){
+        double first = 0;
+        double sec = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Geben Sie die Länge der Seite a ein :");
+        first = input.nextDouble();
+        System.out.print("Geben Sie die Länge der Seite b ein :");
+        sec = input.nextDouble();
+
+        double aire = first * sec;
+        double perimetre = 2 * first + 2 * sec;
+
+        System.out.println("Das Rechteck hat die folgenden Seiten :");
+        System.out.println("Laenge der Seite a: " + first + " cm");
+        System.out.println("Laenge der Seite b: " + sec + " cm");
+        System.out.println("Flaecheninhalt A = " + aire + " qcm");
+        System.out.println("Umfang U = " + perimetre + " cm");
+        input.close();
+    }
+}
