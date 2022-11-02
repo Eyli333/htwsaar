539c2f2ed0a96929f9ca7b84bf95c4ec7b818908
ideflop <vi03pl@gmail.com>
2022-11-02 16:41:27 +0100

    date via import

--- ueb00/name.java
+++ ueb00/name.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.time.Year;
 
 public class name {
 	public static void main( String[] args) {
 		String first ;
 		String second ;
 		int geburtjahr;
+		int current_year = Year.now().getValue();
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Ihr Vorname : " );
@@ -13,7 +15,7 @@ public class name {
 		second = scanner.next();
 		System.out.print("Ihr Geburtsjahr : " );
 		geburtjahr = scanner.nextInt();
-		int alter = 2022 - geburtjahr;
+		int alter = current_year - geburtjahr;
 		System.out.println("Sie heißen : "+ first+ ", " + second);
 		System.out.println("Sie sind " +alter + " Jahre alt");
 		scanner.close();
