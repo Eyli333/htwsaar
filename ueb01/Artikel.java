21717790a5450dbff99e10ad571a066c0abd9e4e
0m3re <100145279+0m3re@users.noreply.github.com>
2022-11-07 12:27:38 +0100

    Update Artikel.java

--- ueb01/Artikel.java
+++ ueb01/Artikel.java
@@ -1,4 +1,5 @@
 /**
+ * Ein Programm fuer eine Artikel Klasse 
  * @author Victor Plage and David Glaser
  * @version 06.11.22
  */
@@ -7,42 +8,84 @@ public class Artikel {
 	private int bestand;
 	private String art;
 
-	public Artikel(int artikelNr, String art, int bestand) { // erster Konstruktor für Artikel
+
+	/*
+	 * erster Konstruktor der Klasse Artikel
+	 * @param artikelNr Die Nummer des Artikels
+	 * @param art Die Art des Artikels
+	 * @param bestand Der Bestand der Artikel
+	 */
+	public Artikel(int artikelNr, String art, int bestand) { 
 		this.artikelNr = artikelNr;
 		this.art = art;
 		this.bestand = bestand;
 	}
 
-	public Artikel(int artikelNr, String art) { // zweiter Konstruktor für Artikel
+	/*
+	 * zweiter Konstruktor der Klasse Artikel
+	 * @param artikelNr Die Nummer des Artikels
+	 * @param art Die Art des Artikels
+	 */
+	public Artikel(int artikelNr, String art) { 
 		this.artikelNr = artikelNr;
 		this.art = art;
 	}
 
+	/*
+	 * Erhoeht den Wert des Bestandes
+	 * @param bestand Der Bestand der Artikel 
+	 */
 	public void bucheZugang(int menge) { // Methode für den Zugang des Bestandes
 		this.bestand += menge;
 	}
 
+	/*
+	 * Veringert den Wert des Bestandes
+	 * @param bestand Der Bestand der Artikel
+	 */
 	public void bucheAbgang(int menge) { // Methode für den Abgang des Bestandes
 		this.bestand -= menge;
 	}
 
-	public String getArt() { // Methode zur Ausgabe der Art
+	/*
+	 * Gibt den Wert des Attributes art aus
+	 * @param art Die art des Artikels
+	 */
+	public String getArt() { 
 		return art;
 	}
 
-	public int getAttribute() { // Methode zur Ausgabe der ArtikelNr
+	/*
+	 * Gibt den Wert des Attributes artikelNr aus
+	 * @param artikelNr Die Nummer des Artikels
+	 */
+	public int getAttribute() { 
 		return artikelNr;
 	}
 
-	public int getBestand() { // Methode zur Ausgabe des Bestandes
+	/*
+	 * Gibt den Wert des Attributes bestand aus
+	 * @param bestand Der Bestand der Artikel
+	 */
+	public int getBestand() { 
 		return bestand;
 	}
 	
-	public void setBestand(int neuerBestand) { // Methode zum setzen eines neuen Bestandes
+	/*
+	 * Setzt den Wert des Attributes bestand
+	 * @param bestand Der Bestand der Artikel
+	 */
+	public void setBestand(int neuerBestand) { 
 		this.bestand = neuerBestand;
 	}
 	
-	public String toString() { // Methode zur Ausgabe der Informationen über den Artikel
+	/*
+	 * Gibt die Werte der Attributen in einem String aus
+	 * @param artikelNr Die Nummer des Artikels
+	 * @param art Die Art des Artikels
+	 * @param bestand Der Bestand der Artikel
+	 */
+	public String toString() { 
 		return "Artikel: " + artikelNr+ ", Art: " + art + ", Bestand: " + bestand;
 	}
 }
