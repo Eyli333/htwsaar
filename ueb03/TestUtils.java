d1f70e7b266251cb9c508078891cf0c30f06d898
0m3re <d2ave@gmx.de>
2022-11-24 18:24:13 +0100

    le bg

--- ueb03/TestUtils.java
+++ ueb03/TestUtils.java
@@ -1,3 +1,11 @@
 public class TestUtils {
-    
+    public static boolean isPositiv(int i){
+        boolean result;
+        if (i > 0){
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
 }
