de77902c1ec3c3eb88bca232245c23ad9030de59
0m3re <100145279+0m3re@users.noreply.github.com>
2022-11-28 15:19:20 +0100

    Wesh

--- ueb04/MathFunctions.java
+++ ueb04/MathFunctions.java
@@ -12,16 +12,16 @@ import java.lang.Math;
 public class MathFunctions{
 
 	private static final double DELTA_NUL = 0.000000001;
-	private static final int zero = 0;
-	private static final int one = 1;
-	private static final int two = 2;
-	private static final int three = 3;
-	private static final int four = 4;
-	private static final int five = 5;
-	private static final int six = 6;
-	private static final int nine = 9;
-	private static final int ten = 10;
-	private static final int eleven = 11;
+	private static final int ZERO = 0;
+	private static final int ONE = 1;
+	private static final int TWO = 2;
+	private static final int THREE = 3;
+	private static final int FOUR = 4;
+	private static final int FIVE = 5;
+	private static final int SIX = 6;
+	private static final int NINE = 9;
+	private static final int TEN = 10;
+	private static final int ELEVEN = 11;
 
 	/**
 	 * Calculate the sum of the divisor for a number
@@ -30,7 +30,7 @@ public class MathFunctions{
 	 * @return  the sum of the divisor
 	 */
 	public static long berechneTeilersumme (long zahl) {
-		TestUtils.checkEqualZero(zahl);
+		TestUtils.checkEqualZERO(zahl);
 		TestUtils.checkIsNonNegativ(zahl);
 
 		long result = 0;
@@ -56,7 +56,7 @@ public class MathFunctions{
 	}
 
 	/**
-	 * Check if the isbn number giving is a valid one 
+	 * Check if the isbn number giving is a valid ONE 
 	 * It just check for ISBN-10
 	 * @param  take the isbn number as a long
 	 * @return X if the cheked digit is 10 else the value of the digit 
@@ -64,31 +64,31 @@ public class MathFunctions{
 	public static String berechneChecksummeIsbn(long isbn) {
 		TestUtils.checkIsbnInput(isbn);
 		
-		long result = zero;
+		long result = ZERO;
 		long isbnMod = isbn;
-		for (int i = nine; i > zero; i--) {
-			long j = isbnMod % ten; // convert to int j
-			isbnMod = isbnMod / ten;
+		for (int i = NINE; i > ZERO; i--) {
+			long j = isbnMod % TEN; // convert to int j
+			isbnMod = isbnMod / TEN;
 			result += j * i;
 
 		}
-		result = result % eleven;
-		if (result == ten) {
+		result = result % ELEVEN;
+		if (result == TEN) {
 			return "X";
 		}
 		return Long.toString(result);
 	}
 
 	/**
-     	* This method is used to Determine the zeros of a quadratic  
+     	* This method is used to Determine the ZEROs of a quadratic  
 	* quadratic equation and return them as a formatted string
      	* @param p is the first value as a double
      	* @param q is the second value as a double
 	* @return x1, x1 and x2 or nothing
      	*/
 	public static String berechneNullstellen (double p, double q) {
-		double delta = Math.pow(p/two, two) - q;
-		double x = -p/two + Math.sqrt(delta);
+		double delta = Math.pow(p/TWO, TWO) - q;
+		double x = -p/TWO + Math.sqrt(delta);
 			if (delta >= -DELTA_NUL && delta < DELTA_NUL) {
 				double x1 = x;
 	 
@@ -100,7 +100,7 @@ public class MathFunctions{
 				
 			} else {
 				double x1 = x;
-				double x2 = -p/two - Math.sqrt(delta);
+				double x2 = -p/TWO - Math.sqrt(delta);
 			
 				return "Zwei Nullstellen: " + x1 + "|" + x2;
 			}
@@ -116,11 +116,11 @@ public class MathFunctions{
 	public static int berechneGgt(int zahl1, int zahl2) {
 		TestUtils.checkIsNonNegativ(zahl1);
 		TestUtils.checkIsNonNegativ(zahl2);
-		TestUtils.checkBothNumbersZero(zahl1, zahl2);
+		TestUtils.checkBothNumbersZERO(zahl1, zahl2);
 
-		if (zahl1 == zero) {
+		if (zahl1 == ZERO) {
 			return zahl2;
-		} else if (zahl2 == zero) {
+		} else if (zahl2 == ZERO) {
 			return zahl1;
 		}
 		
@@ -130,7 +130,7 @@ public class MathFunctions{
 			zahl1 = temp;	
 		}
 
-		while (zahl2 != zero) {
+		while (zahl2 != ZERO) {
 			int temp = zahl1;
 			zahl1 = zahl2;
 			zahl2 = temp % zahl1;
@@ -149,22 +149,22 @@ public class MathFunctions{
      	*/
 	public static long berechneFakultaet(long zahl){
 		TestUtils.checkIsNonNegativ(zahl);
-		TestUtils.checkAboveTwentsix(zahl);
+		TestUtils.checkAboveTwentSIX(zahl);
 
-		if (zahl == zero || zahl == one) {
+		if (zahl == ZERO || zahl == ONE) {
 
-			return one;
+			return ONE;
 
 			// return BigInteger.valueOf(1);
 		}
 		
 		// else if (zahl < 26) {
 		
-		long sum = one;
+		long sum = ONE;
 
-		while (zahl > one) {
+		while (zahl > ONE) {
 			sum *= zahl; 
-			zahl -= one;
+			zahl -= ONE;
 		}
 		
 		return sum;
@@ -189,7 +189,7 @@ public class MathFunctions{
 	* @return true if it's possible else false
      	*/
 	public static boolean istSummeVonPotenzen(long zahl) {
-		TestUtils.checkEqualZero(zahl);
+		TestUtils.checkEqualZERO(zahl);
 		TestUtils.checkIsNonNegativ(zahl);
 
 		double doubleZahl = (double)zahl;
@@ -201,13 +201,13 @@ public class MathFunctions{
 		long cubeRootZahl = (long)cubeRootZahlDouble;
 		long quarticRootZahl = (long)quarticRootZahlDouble;
 
-		for (long i = one; i <= quarticRootZahl; ++i) {
-			for (long j = one; j <= cubeRootZahl; ++j) {
-				for (long k = one; k <= squareRootZahl; ++k) {
+		for (long i = ONE; i <= quarticRootZahl; ++i) {
+			for (long j = ONE; j <= cubeRootZahl; ++j) {
+				for (long k = ONE; k <= squareRootZahl; ++k) {
 
-					double a = Math.pow((double)i, (double) four); 
-					double b = Math.pow((double)j, (double) three);
-					double c = Math.pow((double)k, (double) two);
+					double a = Math.pow((double)i, (double) FOUR); 
+					double b = Math.pow((double)j, (double) THREE);
+					double c = Math.pow((double)k, (double) TWO);
 					double compute = a + b + c;
 
 					if ((long)compute == zahl) {
@@ -227,12 +227,12 @@ public class MathFunctions{
 	* @return the sum as a double
      	*/
 	public static double berechneReihensumme(int anzahl, double x) {
-		TestUtils.checkEqualZero(anzahl);
+		TestUtils.checkEqualZERO(anzahl);
 		TestUtils.checkIsNonNegativ(anzahl);
 		
-		double sum = zero;
-		for (int i = one; i <= anzahl; ++i) {
-			double over = Math.pow(x-one, i);
+		double sum = ZERO;
+		for (int i = ONE; i <= anzahl; ++i) {
+			double over = Math.pow(x-ONE, i);
 			double under = i * Math.pow(x, i);
 			sum += over / under;
 		}
@@ -245,23 +245,23 @@ public class MathFunctions{
 	* @return the result as a boolean
      	*/
 	public static boolean istPrimzahl(long zahl) {
-		TestUtils.checkEqualZero(zahl);
+		TestUtils.checkEqualZERO(zahl);
 		TestUtils.checkIsNonNegativ(zahl);
 
-		if (zahl <= three){
+		if (zahl <= THREE){
 
 			return true;
 		}
 		// This is checked so that we can skip
-		// middle five numbers in below loop
-		if (zahl % two == zero || zahl % three == zero){
+		// middle FIVE numbers in below loop
+		if (zahl % TWO == ZERO || zahl % THREE == ZERO){
 
 			return false;
 		}
 
-		for (int i = five; i * i <= zahl; i = i + six){
+		for (int i = FIVE; i * i <= zahl; i = i + SIX){
 
-			if (zahl % i == zero || zahl % (i + two) == zero){
+			if (zahl % i == ZERO || zahl % (i + TWO) == ZERO){
 
 				return false;
 			}
