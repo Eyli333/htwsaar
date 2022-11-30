f256d3cb49b08a17a8deb99582f3201a83891e23
0m3re <d2ave@gmx.de>
2022-11-30 16:04:44 +0100

    fix

--- ueb04/MathFunctions.java
+++ ueb04/MathFunctions.java
@@ -142,11 +142,7 @@ public class MathFunctions{
 		if (zahl == 0 || zahl == 1) {
 
 			return 1;
-
-			// return BigInteger.valueOf(1);
 		}
-		
-		// else if (zahl < 26) {
 		
 		long sum = 1;
 
@@ -156,19 +152,6 @@ public class MathFunctions{
 		}
 		
 		return sum;
-
-		// return BigInteger.valueOf(sum);
-
-		// }
-	
-		// BigInteger sum = new BigInteger("1");
-
-		// while(zahl > 1){
-		// 	sum = sum.multiply(BigInteger.valueOf(zahl));
-		// 	zahl -= 1;
-		// }
-
-		// return sum;
 	}
 	/**
 	 * This method is used to calulate to check if a number
