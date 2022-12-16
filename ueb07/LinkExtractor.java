4c6df8f68d467a34c6e72ea788d52cb2ba959ec2
ideflop <vi03pl@gmail.com>
2022-12-16 21:53:12 +0100

    add Unit test

--- ueb07/LinkExtractor.java
+++ ueb07/LinkExtractor.java
@@ -1,9 +1,16 @@
+/**
+ * This class is used to extract links from a given input and display their text and URL.
+ * It also counts the number of links found and the number of lines in the input.
+ * 
+ * @author Victor Plage and David Glaser
+ * @version 1.1.0
+ * @since 2022-12-13
+ */
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinkExtractor {
-    //private static final String LINK_REGEX = "<a href=\"(.+?)\">(.+?)</a>";
     private static final String LINK_REGEX = "<\\s*a.*?href\\s*=\\s*\"\\s*(.+?)\\s*\".*?>\\s*(.+?)\\s*</a>";
     private static final Pattern LINK_PATTERN = Pattern.compile(LINK_REGEX);
 
@@ -11,35 +18,46 @@ public class LinkExtractor {
     int lineCount = 0;
 
     Scanner scanner = new Scanner(System.in);
-    
+
+    /**
+     * Prints a formatted string to the console.
+     * @param format format string
+     * @param args arguments for the format string
+     */
     public void printf(String format, Object... args) {
         System.out.printf(format + "\n", args);
     }
 
+    /**
+     * Main method for the LinkExtractor class.
+     * @param args command line arguments (unused)
+     */
     public static void main(String[] args) {
         new LinkExtractor().run();
     }
 
+    /**
+     * Extracts links from the input and displays their text and URL.
+     */
     public void run() {
-
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             lineCount++;
 
             Matcher linkMatcher = LINK_PATTERN.matcher(line);
             if (linkMatcher.find()) {
-                linkCount++;
-
                 String linkUrl = linkMatcher.group(1);
 
                 if (linkUrl.equals(linkUrl.toLowerCase())) {
+                    linkCount++;
                     String linkText = linkMatcher.group(2);
+
                     printf("%s: %s, Anzahl Zeichen: %d", linkText, linkUrl, linkUrl.length());
                 }
             }
         }
 
-        printf("%d Links wurden in %d Zeilen gefunden", linkCount, lineCount);
+        printf("%d Links wurden in %d Zeilen gefunden.", linkCount, lineCount);
 
         scanner.close();
     }
---
+++ ueb07/LinkExtractorTest.java
@@ -0,0 +1,286 @@
+/**
+ * JUnit test class for the LinkExtractor class.
+ *
+ * @author Victor Plage and David Glaser
+ * @version 1.1.0
+ * @since 2022-12-13
+ */
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LinkExtractorTest {
+    private LinkExtractor extractor = new LinkExtractor();
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    /**
+     * Helper method for running the LinkExtractor's `run()` method with a given test input.
+     * @param testInput the test input to pass to the LinkExtractor
+     */
+    private void runExtractor(String testInput) {
+        try (ByteArrayInputStream in = new ByteArrayInputStream(testInput.getBytes())){
+            extractor.scanner = new Scanner(in);
+            extractor.run();
+        } catch (IOException e) {
+            fail("IOException occurred: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Method called before each test to redirect test output to the outContent stream.
+     */
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    /**
+     * Method called after each test to restore normal output.
+     */
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    /**
+     * Tests that the LinkExtractor's `linkCount` and `lineCount` fields are set correctly
+     * for normal hypertext links.
+     */
+    @Test
+    public void normal_hypertextlinks_expected_value2_linkCount_lineCount() {
+        String testInput = "<a href=\"http://example.com\">Example</a>\n<a href=\"http://test.com\">Test</a>";
+        int expectedLinkCount = 2;
+        int expectedLineCount = 2;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+    /**
+     * Tests that the LinkExtractor's `printf()` method produces the correct output
+     * for normal hypertext links.
+     */
+    @Test
+    public void printf_output_correct_normal_hypertextlinks() {
+        String linkText1 = "Example";
+        String linkUrl1 = "http://example.com";
+        int linkUrlLength1 = linkUrl1.length();
+
+        String linkText2 = "Test";
+        String linkUrl2 = "http://test.com";
+        int linkUrlLength2 = linkUrl2.length();
+
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText1, linkUrl1, linkUrlLength1);
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText2, linkUrl2, linkUrlLength2);
+
+        String expectedOutput = String.format("%s: %s, Anzahl Zeichen: %d\n%s: %s, Anzahl Zeichen: %d\n", linkText1, linkUrl1, linkUrlLength1, linkText2, linkUrl2, linkUrlLength2);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Tests that the LinkExtractor's `run` method produces the correct output
+     * for normal hypertext links.
+     */
+    @Test
+    public void printf_run_output_normal_hypertextlinks() {
+        String testInput = "<a href=\"http://example.com\">Example</a>\n<a href=\"http://test.com\">Test</a>";
+        int expectedLinkCount = 2;
+        int expectedLineCount = 2;
+
+        String linkText1 = "Example";
+        String linkUrl1 = "http://example.com";
+        int linkUrlLength1 = linkUrl1.length();
+
+        String linkText2 = "Test";
+        String linkUrl2 = "http://test.com";
+        int linkUrlLength2 = linkUrl2.length();
+
+        String expectedOutput = String.format("%s: %s, Anzahl Zeichen: %d\n%s: %s, Anzahl Zeichen: %d\n%d Links wurden in %d Zeilen gefunden.\n", linkText1, linkUrl1, linkUrlLength1, linkText2, linkUrl2, linkUrlLength2, expectedLinkCount, expectedLineCount);
+        
+        runExtractor(testInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Tests the LinkExtractor with multiple hypertext links.
+     * Expected values: linkCount = 3, lineCount = 5.
+     */
+    @Test
+    public void multiple_hypertextlinks_expected_value3_linkCount_value5_lineCount() {
+        String testInput = "<p>\n" +
+            "  <li><a href=\"http://www.htwsaar.de/\">htwsaar</a></li>\n" +
+            "  <li><a href=\"http://www.google.com\">Google</a></li>\n" +
+            "  <a href=\"http://www.heise.de\">Heise Online</a>\n" +
+            "</p>";
+        int expectedLinkCount = 3;
+        int expectedLineCount = 5;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+    /**
+     * Tests the LinkExtractor's printf method with multiple hypertext links.
+     * Verifies that the output is correct.
+     */
+    @Test
+    public void printf_output_correct_multiple_hypertextlinks() {
+        String linkText1 = "htwsaar";
+        String linkUrl1 = "http://www.htwsaar.de/";
+        int linkUrlLength1 = linkUrl1.length();
+
+        String linkText2 = "Google";
+        String linkUrl2 = "http://www.google.com";
+        int linkUrlLength2 = linkUrl2.length();
+
+        String linkText3 = "Heise Online";
+        String linkUrl3 = "http://www.heise.de";
+        int linkUrlLength3 = linkUrl3.length();
+
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText1, linkUrl1, linkUrlLength1);
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText2, linkUrl2, linkUrlLength2);
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText3, linkUrl3, linkUrlLength3);
+
+        String expectedOutput = String.format("%s: %s, Anzahl Zeichen: %d\n%s: %s, Anzahl Zeichen: %d\n%s: %s, Anzahl Zeichen: %d\n", linkText1, linkUrl1, linkUrlLength1, linkText2, linkUrl2, linkUrlLength2, linkText3, linkUrl3, linkUrlLength3);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Tests the LinkExtractor with a hypertext link that has a class attribute.
+     * Expected values: linkCount = 1, lineCount = 1.
+     */
+    @Test
+    public void hypertextlink_with_class_attribute_expected_value1_linkCount_lineCount() {
+        String testInput = "<li><a class=\"hello\" href=\"http://www.htwsaar.de/\">htwsaar</a></li>";
+        int expectedLinkCount = 1;
+        int expectedLineCount = 1;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+    /**
+     * Tests the LinkExtractor's printf method with a hypertext link that has a class attribute.
+     * Verifies that the output is correct.
+     */
+    @Test
+    public void printf_output_correct_hypertextlink_with_class_attribute() {
+        String linkText = "htwsaar";
+        String linkUrl = "http://www.htwsaar.de/";
+        int linkUrlLength = linkUrl.length();
+
+        extractor.printf("%s: %s, Anzahl Zeichen: %d", linkText, linkUrl, linkUrlLength);
+
+        String expectedOutput = String.format("%s: %s, Anzahl Zeichen: %d\n", linkText, linkUrl, linkUrlLength);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Tests the LinkExtractor with a hypertext link that has a class attribute at the end.
+     * Expected values: linkCount = 1, lineCount = 1.
+     */
+    @Test
+    public void hypertextlink_with_class_attribute_at_end_expected_value1_linkCount_lineCount() {
+        String testInput = "<li><a href=\"http://www.htwsaar.de/\" class=\"hello\">htwsaar</a></li>";
+        int expectedLinkCount = 1;
+        int expectedLineCount = 1;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+    /**
+     * Tests the LinkExtractor with a hypertext link that has a target attribute.
+     * Expected values: linkCount = 1, lineCount = 1.
+     */
+    @Test
+    public void hypertextlink_with_target_attribute_expected_value1_linkCount_lineCount() {
+        String testInput = "<li><a target=\"_blank\" href=\"http://www.htwsaar.de/\">htwsaar</a></li>";
+        int expectedLinkCount = 1;
+        int expectedLineCount = 1;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+    /**
+     * Tests the LinkExtractor with a hypertext link that has a target attribute at the end.
+     * Expected values: linkCount = 1, lineCount = 1.
+     */
+    @Test
+    public void hypertextlink_with_target_attribute_at_end_expected_value1_linkCount_lineCount() {
+        String testInput = "<li><a href=\"http://www.htwsaar.de/\" target=\"_blank\">htwsaar</a></li>";
+        int expectedLinkCount = 1;
+        int expectedLineCount = 1;
+
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+    }
+
+   /**
+     * Tests the LinkExtractor with a hypertext link that has an uppercase "http" value.
+     * Expected values: linkCount = 0, lineCount = 1.
+     */
+    @Test
+    public void hypertextlink_with_uppercase_http_expected_value0_linkCount_lineCount() {
+        String testInput = "<li><a href=\"Http://www.htwsaar.de/\">htwsaar</a></li>";
+        int expectedLinkCount = 0;
+        int expectedLineCount = 1;
+        runExtractor(testInput);
+
+        assertEquals(expectedLinkCount, extractor.linkCount);
+        assertEquals(expectedLineCount, extractor.lineCount);
+
+    }
+
+    /**
+     * Tests the LinkExtractor's printf method with a hypertext link that has an uppercase "http" value.
+     * Verifies that the output is correct.
+     */
+    @Test
+    public void printf_run_output_hypertextlink_with_uppercase_http() {
+        String testInput = "<li><a href=\"Http://www.htwsaar.de/\">htwsaar</a></li>";
+        int expectedLinkCount = 0;
+        int expectedLineCount = 1;
+
+        String expectedOutput = String.format("%d Links wurden in %d Zeilen gefunden.\n", expectedLinkCount, expectedLineCount);
+        
+        runExtractor(testInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    /**
+     * Tests the LinkExtractor's printf method with a hypertext link on two lines.
+     * Verifies that the output is correct.
+     */
+    @Test
+    public void printf_run_output_with_hypertextlinks_on_2_lines() {
+        String testInput = "<a href=\"http://example.com\"\n>Example</a>";
+        int expectedLinkCount = 0;
+        int expectedLineCount = 2;
+
+        String expectedOutput = String.format("%d Links wurden in %d Zeilen gefunden.\n", expectedLinkCount, expectedLineCount);
+        
+        runExtractor(testInput);
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
