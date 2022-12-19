3338e53e744d1ece8e881c64b2dde2f3714f7206
ideflop <vi03pl@gmail.com>
2022-12-19 12:51:04 +0100

    check for space in </a>

--- ueb07/LinkExtractor.java
+++ ueb07/LinkExtractor.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LinkExtractor {
-    private static final String LINK_REGEX = "<\\s*a.*?href\\s*=\\s*\"\\s*(.+?)\\s*\".*?>\\s*(.+?)\\s*</a>";
+    private static final String LINK_REGEX = "<\\s*a.*?href\\s*=\\s*\"\\s*(.+?)\\s*\".*?>\\s*(.+?)\\s*<\\s*/a\\s*>";
     private static final Pattern LINK_PATTERN = Pattern.compile(LINK_REGEX);
 
     int linkCount = 0;
